8385c2ae11a36c250b281b39ceca48b35739c9a7
Andrea Perugini <alp.perugini@gmail.com>
2024-12-04 00:00:19 +0100

    added running workout tracking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.anychart.android)
+    implementation("org.osmdroid:osmdroid-android:6.1.20")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
