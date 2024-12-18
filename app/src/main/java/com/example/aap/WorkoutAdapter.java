b7da5dd3b8db916597eee0eef1009e22d6b4b626
Andrea Perugini <alp.perugini@gmail.com>
2024-12-18 01:15:43 +0100

    ui improvements

--- app/src/main/java/com/example/aap/WorkoutAdapter.java
+++ app/src/main/java/com/example/aap/WorkoutAdapter.java
@@ -40,7 +40,7 @@ public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.WorkoutV
     @Override
     public void onBindViewHolder(@NonNull WorkoutViewHolder holder, int position) {
         Workout workout = workouts.get(position);
-        holder.dateTextView.setText("Date: " + workout.getDate());
+        holder.dateTextView.setText("" + workout.getDate());
         holder.distanceTextView.setText(String.format("Distance: %.2f km", workout.getDistance() / 1000));
         holder.timeTextView.setText(String.format("Time: %d min", workout.getTime() / 60000));
         holder.avgSpeedTextView.setText(String.format("Avg Speed: %.2f km/h", workout.getAverageSpeed()));
